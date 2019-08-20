8938f96f94a21ce1ee294d8fb99e37f299fc6fc6
Matthias Kalb <matthias.kalb@railsmechanic.de>
2019-08-20 16:21:27 +0200

    Changed API version to 1119

--- lib/ebay_client/configuration.rb
+++ lib/ebay_client/configuration.rb
@@ -14,8 +14,8 @@ class EbayClient::Configuration
 
   def initialize(configuration={})
     raise ArgumentError.new('Configuration must be a Hash') unless configuration.is_a?(Hash)
-    @version            = configuration.fetch(:version, 997)
-    @wsdl_file          = configuration.fetch(:wsdl_file, 'https://raw.githubusercontent.com/railsmechanic/ebay_client/master/vendor/ebay/997.wsdl')
+    @version            = configuration.fetch(:version, 1119)
+    @wsdl_file          = configuration.fetch(:wsdl_file, 'http://developer.ebay.com/webservices/1119/eBaySvc.wsdl')
     @siteid             = configuration.fetch(:siteid, 0)
     @routing            = configuration.fetch(:routing, 'default')
     @url                = configuration.fetch(:url, 'https://api.sandbox.ebay.com/wsapi')
